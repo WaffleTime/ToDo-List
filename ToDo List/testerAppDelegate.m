c25cca97a34f4f08a5fa33bdcea2ec3bad4b2de9
clblamires <superstudent@tristans-iphone.lcsc.edu>
2013-09-18 13:27:45 -0700

    asdasd

--- ToDo List/testerAppDelegate.m
+++ ToDo List/testerAppDelegate.m
@@ -10,6 +10,8 @@
 
 @implementation testerAppDelegate
 
+//THis is a test
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
